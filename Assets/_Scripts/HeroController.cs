4aee0f7e7f1736fe2477ad3a0b31f441926afed9
jagpreetjattana <jagpreet@hotmail.com>
2016-02-27 01:09:19 -0500

    Added Sprite

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -16,7 +16,7 @@ public class VelocityRange
     }
 
 }
-
+/*
 public class HeroController : MonoBehaviour {
     //private instance variable
     private Animator _animator;
@@ -33,6 +33,7 @@ public class HeroController : MonoBehaviour {
     public float moveForce;
     public float jumpForce;
     public Transform groundCheck;
+    public Transform camera;
 
 
 
@@ -40,9 +41,9 @@ public class HeroController : MonoBehaviour {
     void Start () {
         //Initialize public instance varibles
         this.velocityRange = new VelocityRange(300f, 25000f);
-       // this.moveForce = 50f;
+        // this.moveForce = 50f;
         //this.jumpForce = 500f;
-        
+       // this._spawn();
 
         //Initialize private instance variables
         this._transform = gameObject.GetComponent<Transform>();
@@ -51,15 +52,17 @@ public class HeroController : MonoBehaviour {
         this._jump = 0f;
         this._facingRight = true;
         this._rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
-
+      //  this._spawn();
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
+        Vector3 currentCameraPosition = new Vector3(this._transform.position.x, this._transform.position.y,-10f);
+        this.camera.position = currentCameraPosition;
         this._isGrounded = Physics2D.Linecast(this._transform.position, this.groundCheck.position,1<<LayerMask.NameToLayer("Ground"));
         Debug.DrawLine(this._transform.position, this.groundCheck.position);
-
+        
      
         float forceX = 0f;
         float forceY = 0f;
@@ -135,6 +138,14 @@ public class HeroController : MonoBehaviour {
 
     }
 
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Death")) {
+          //  this._spawn();
+        }
+
+
+    }
+
     // PRIVATE METHODS
     private void _flip()
     {
@@ -147,4 +158,152 @@ public class HeroController : MonoBehaviour {
         }
     }
 
+    private void _spawn()
+    {
+        this._transform.position = new Vector3(-125f, 125f, 0);
+    }
+
+}*/
+public class HeroController : MonoBehaviour
+{
+
+    // PUBLIC INSTANCE VARIABLES
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
+    public Transform camera;
+   // public GameController gameController;
+
+    // PRIVATE  INSTANCE VARIABLES
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+    private Rigidbody2D _rigidBody2D;
+    private bool _isGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize Public Instance Variables
+        this.velocityRange = new VelocityRange(300f, 30000f);
+
+        // Initialize Private Instance Variables
+        this._transform = gameObject.GetComponent<Transform>();
+        this._animator = gameObject.GetComponent<Animator>();
+        this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        this._move = 0f;
+        this._jump = 0f;
+        this._facingRight = true;
+
+        // place the hero in the starting position
+        this._spawn();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = new Vector3(this._transform.position.x, this._transform.position.y, -10f);
+        this.camera.position = currentPosition;
+
+        this._isGrounded = Physics2D.Linecast(
+            this._transform.position,
+            this.groundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawLine(this._transform.position, this.groundCheck.position);
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        // get absolute value of velocity for our gameObject
+        float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
+
+        // Ensure the player is grounded before any movement checks
+        if (this._isGrounded)
+        {
+            // gets a number between -1 to 1 for both Horizontal and Vertical Axes
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+
+            if (this._move != 0)
+            {
+                if (this._move > 0)
+                {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                    this._flip();
+                }
+                if (this._move < 0)
+                {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+                    this._facingRight = false;
+                    this._flip();
+                }
+
+                // call the walk clip
+                this._animator.SetInteger("Anim_State", 1);
+            }
+            else {
+
+                // set default animation state to "idle"
+                this._animator.SetInteger("Anim_State", 0);
+            }
+
+            if (this._jump > 0)
+            {
+                // jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
+
+            }
+        }
+        else {
+            // call the "jump" clip
+            this._animator.SetInteger("Anim_State", 2);
+        }
+
+        // Apply the forces to the player
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            Debug.Log("death");
+            this._spawn();
+           // this.gameController.LivesValue--;
+        }
+    }
+
+    // PRIVATE METHODS
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale = new Vector2(1, 1);
+        }
+        else {
+            this._transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    private void _spawn()
+    {
+        this._transform.position = new Vector3(-125f, 125f, 0);
+    }
 }
